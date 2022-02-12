74a62dd7dce8987e1b35fb3dd0f642fa4682a8da
Mineur <williamlyu96@gmail.com>
2022-02-12 15:25:41 +0100

    Update 20. Valid Parentheses.py

--- Stack/20. Valid Parentheses.py
+++ Stack/20. Valid Parentheses.py
@@ -1,1 +1,10 @@
-
+class Solution:
+    def isValid(self, s: str) -> bool:
+        m = {')':'(', ']':'[', '}':'{'}
+        stack = []
+        for c in s:
+            if c not in m:
+                stack.append(c)
+            elif len(stack) == 0 or m[c] != stack.pop():
+                return False
+        return stack == []
