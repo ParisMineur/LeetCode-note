8e68ae2051352ffa7dc7b483b7e968618934fddf
Mineur <williamlyu96@gmail.com>
2022-02-16 16:52:48 +0100

    Update 70. Climbing Stairs.py

--- Dynamic programming/70. Climbing Stairs.py
+++ Dynamic programming/70. Climbing Stairs.py
@@ -11,3 +11,22 @@ class Solution:
             a = b-a
             
         return b
+
+    
+## second solution
+
+'''
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+        
+        dp = [0]*(n+1)
+        dp[1], dp[2] = 1, 2
+        
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        
+        return dp[n]
+
+'''
