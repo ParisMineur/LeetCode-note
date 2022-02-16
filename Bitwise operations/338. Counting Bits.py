485e0540379910fe407e3743792be2ea54e3cf4a
Mineur <williamlyu96@gmail.com>
2022-02-16 15:36:09 +0100

    Update 338. Counting Bits.py

--- Bitwise operations/338. Counting Bits.py
+++ Bitwise operations/338. Counting Bits.py
@@ -10,3 +10,16 @@ class Solution:
             res.append(count)
         
         return res
+    
+## second solution
+'''
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        res = [0]*(n+1)
+        
+        for i in range(1, n+1):
+            res[i] = res[i&(i-1)] + 1
+            
+        return res
+
+'''
