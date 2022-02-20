1d78f9e0fba168b60ad5ce4ef184386010f838c6
Mineur <williamlyu96@gmail.com>
2022-02-20 22:51:39 +0100

    Update 116. Populating Next Right Pointers in Each Node.py

--- BFS/116. Populating Next Right Pointers in Each Node.py
+++ BFS/116. Populating Next Right Pointers in Each Node.py
@@ -17,3 +17,34 @@ class Solution:
                 d.append(node.right)
 
         return root
+
+    
+    ## second solution, same to "117. Populating Next Right Pointers in Each Node II"
+    '''
+    class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if not root:
+            return
+        
+        d = collections.deque()
+        d.append(root)
+        res = []
+        
+        while d:
+            temp = []
+            
+            for _ in range(len(d)):
+                node = d.popleft()
+                temp.append(node)
+                
+                d.append(node.left) if node.left else 0
+                d.append(node.right) if node.right else 0
+                    
+            res.append(temp)
+            
+        for a in res:
+            for i in range(len(a)-1):
+                a[i].next = a[i+1]
+                
+        return root
+    '''
